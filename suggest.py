efa4853e1033b1c3d66ec6ffdd110c1793122096
Matt C <mcompton2002@gmail.com>
2020-01-23 21:54:05 +0000

    Made suggest send emails to owner

--- suggest.py
+++ suggest.py
@@ -1,10 +1,17 @@
 import os
 
+from emc import mailer
+with open("../pass") as f:
+    passw = f.read()
+m = mailer("mcompton2002@gmail.com",passw)
+
 n = input("Your name (or nickname, I don't judge): ")
 c = input("Some form of contact info, if I want to follow up (optional): ")
 s = input("Your suggestion, " + n + ": ")
 
 sgst = "From: " + n + "\nContact info: " + c + "\nSuggestion: " + s 
+
+m.mkmessage("3019745990@vtext.com","Suggestion from " + name,sgst)
 
 with open("suggestions.txt","a+") as f:
     f.write(sgst + "\n")
